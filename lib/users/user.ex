b60dcd256456596721c6f4ec0c8b07f8850791bd
brandaoplaster <brandaoplaster@gmail.com>
2021-04-25 17:02:30 -0300

    feat: Adding struct user

---
+++ lib/users/user.ex
@@ -0,0 +1,17 @@
+defmodule FlightReservations.Users.User do
+  @keys [:name, :email, :cpf]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, cpf) when is_binary(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf
+     }}
+  end
+
+  def build(_name, _email, _cpf), do: {:error, "Invalid params"}
+end
