c659f24e642eed905b1d0642baea309b48f5e923
brandaoplaster <brandaoplaster@gmail.com>
2021-04-27 07:36:56 -0300

    feat: Adding Agent for booking

---
+++ lib/bookings/agent.ex
@@ -0,0 +1,27 @@
+defmodule FlightReservations.Bookings.Agent do
+  use Agent
+
+  alias FlightReservations.Bookings.Booking
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Booking{} = booking) do
+    Agent.update(__MODULE__, &update_state(&1, booking))
+    {:ok, booking.id}
+  end
+
+  def get(id), do: Agent.get(__MODULE__, &get_booking(&1, id))
+
+  defp get_booking(state, id) do
+    case Map.get(state, id) do
+      nil -> {:error, "Booking not found"}
+      booking -> {:ok, booking}
+    end
+  end
+
+  defp update_state(state, %Booking{id: id} = booking) do
+    Map.put(state, id, booking)
+  end
+end
