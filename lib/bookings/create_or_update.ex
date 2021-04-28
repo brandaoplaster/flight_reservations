7018bdab0a0260583f72033bc237e8b04e1e5821
brandaoplaster <brandaoplaster@gmail.com>
2021-04-28 10:51:56 -0300

    refactor: Removing redundant funcation

--- lib/bookings/create_or_update.ex
+++ lib/bookings/create_or_update.ex
@@ -1,30 +1,17 @@
 defmodule FlightReservations.Bookings.CreateOrUpdate do
-  alias FlightReservations.Users.Agent, as: UserAgent
   alias FlightReservations.Bookings.Agent, as: BookingAgent
-  alias FlightReservations.Users.User
   alias FlightReservations.Bookings.Booking
 
-  def call(%{user_id: user_id, booking: booking}) do
-    with {:ok, user} <- UserAgent.get_user_by_id(user_id),
-         {:ok, booking} <- build_booking(booking, user) do
+  def call(%{
+        user_id: user_id,
+        complete_date: complete_date,
+        origin_city: origin_city,
+        destination_city: destination_city
+      }) do
+    with {:ok, booking} <- Booking.build(complete_date, origin_city, destination_city, user_id) do
       BookingAgent.save(booking)
     else
       error -> error
-    end
-  end
-
-  defp build_booking(
-         %{
-           complete_date: complete_date,
-           origin_city: origin_city,
-           destination_city: destination_city,
-           user_id: user_id
-         },
-         %User{id: user_id}
-       ) do
-    case Booking.build(complete_date, origin_city, destination_city, user_id) do
-      {:ok, booking} -> booking
-      {:error, _reason} = error -> error
     end
   end
 end
