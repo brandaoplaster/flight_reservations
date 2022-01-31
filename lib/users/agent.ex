9c2f65264366129aeae4b82cb9ee8d1af0208199
brandaoplaster <brandaoplaster@gmail.com>
2022-01-30 22:09:59 -0300

    refactor: Removing unused functions

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -14,21 +14,12 @@ defmodule FlightReservations.Users.Agent do
 
   def get_user_by_cpf(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
 
-  def get_user_by_id(id), do: Agent.get(__MODULE__, &get_user_id(&1, id))
-
   defp update_state(state, %User{cpf: cpf} = user) do
     Map.put(state, cpf, user)
   end
 
   defp get_user(state, cpf) do
     case Map.get(state, cpf) do
-      nil -> {:error, "User not found"}
-      user -> {:ok, user}
-    end
-  end
-
-  defp get_user_id(state, id) do
-    case Map.get(state, id) do
       nil -> {:error, "User not found"}
       user -> {:ok, user}
     end
